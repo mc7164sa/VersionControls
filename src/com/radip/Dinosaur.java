205fdee5a6a2b063dff0ae0f60be7d77a1208d3d
Radip <radipmanandhar@gmail.com>
2016-02-18 18:05:16 -0600

    Started Dinosaur program

---
+++ src/com/radip/Dinosaur.java
@@ -0,0 +1,10 @@
+package com.radip;
+
+/**
+ * Created by Radip on 2/18/2016.
+ */
+public class Dinosaur {
+    public void roar(){
+        System.out.println("GRRR!!!!!");
+    }
+}
---
+++ src/com/radip/DinosaurSupervisor.java
@@ -0,0 +1,14 @@
+package com.radip;
+
+public class DinosaurSupervisor {
+
+    public static void main(String[] args) {
+
+	Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+
+        t_rex.roar();
+        velociraptor.roar();
+
+    }
+}
